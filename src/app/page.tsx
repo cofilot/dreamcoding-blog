4860c12997940e8d17bd0b960cf3f541a1122ff5
cofilot <shcoshcoshco@gmail.com>
2023-04-15 17:30:15 +0900

    fix: using async/await inside components other than pages and layouts results in type error in TypeScript yet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ export default function Home() {
   return (
     <section>
       <Hero />
+      {/* https://beta.nextjs.org/docs/configuring/typescript#async-server-component-typescript-error */}
+      {/* @ts-expect-error Server Component */}
       <FeaturedPosts />
     </section>
   );
